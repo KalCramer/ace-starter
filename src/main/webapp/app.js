61042a191126c620e02ae0408bc51d84bc0c5cff
Kal Cramer <kal.cramer@connexta.com>
2019-12-12 16:59:16 -0700

    test

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,5 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Contacts from './components/missions';
+import axios from 'axios'
 
-export default () => {
-  return <h1>hello, world</h1>
+
+class App extends Component {
+    render() {
+        return (
+            <Contacts contacts={this.state.contacts} />
+        )
+    }
+
+    state = {
+        contacts: []
+    };
+
+    componentDidMount() {
+    let headers = new Headers();
+    headers.set('Authorization', 'Basic ' + "YWRtaW46YWRtaW4=");
+    headers.set('Referer', 'https://localhost:8080/services/');
+
+
+axios.get('healthstatus/pulse/v1/missions', {
+ headers: {
+   Authorization: 'Basic ' + "YWRtaW46YWRtaW4=",
+   Origin: 'https://localhost:8993/',
+   'X-Requested-With': 'XmlHttpRequest'}
+})    .then(response => this.setState({contacts: response.data}))
+
+
+ /*   fetch('https://localhost:8993/services/healthstatus/pulse/v1/missions',
+        {method:'GET',
+         headers: headers,
+        })
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data)
+                this.setState({ contacts: data })
+            })
+            .catch(console.log)*/
+    }
 }
+
+export default App;
