5e36e85acd0968a2666f8be129f647416d933a89
Kal Cramer <kal.cramer@connexta.com>
2020-01-03 13:50:46 -0700

    add table and have some data from routes on it

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,200 +1,192 @@
 import React, { Component } from 'react'
-import Contacts from './components/missions';
-import axios from 'axios'
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import VectorLayer from 'ol/layer/Vector';
-import GeoJSON from 'ol/format/GeoJSON';
-import {OSM, Vector as VectorSource} from 'ol/source';
-import {fromLonLat} from 'ol/proj';
+import Map from 'ol/Map'
+import View from 'ol/View'
+import TileLayer from 'ol/layer/Tile'
+import XYZ from 'ol/source/XYZ'
+import VectorLayer from 'ol/layer/Vector'
+import GeoJSON from 'ol/format/GeoJSON'
+import { OSM, Vector as VectorSource } from 'ol/source'
+import { fromLonLat } from 'ol/proj'
+import RouteTable from './components/missions'
 
-import Feature from 'ol/Feature';
-import Polygon from 'ol/geom/Polygon';
-import Point from 'ol/geom/Point';
-import LineString from 'ol/geom/LineString';
+import Feature from 'ol/Feature'
+import Polygon from 'ol/geom/Polygon'
+import Point from 'ol/geom/Point'
+import LineString from 'ol/geom/LineString'
 
-import Style from 'ol/style/Style';
-import Circle from 'ol/style/Circle';
-import Fill from 'ol/style/Fill';
-import Stroke from 'ol/style/Stroke';
-
-
+import Style from 'ol/style/Style'
+import Circle from 'ol/style/Circle'
+import Fill from 'ol/style/Fill'
+import Stroke from 'ol/style/Stroke'
 
 class MapWrapper extends Component {
-    constructor(props){
+  constructor(props) {
     super(props)
-    console.log(props)
-        this.state = {}
-    }
-
-
+    this.state = {}
+  }
 
   componentDidMount() {
-
     var raster = new TileLayer({
-    source: new XYZ({
-                url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-              })
-    });
+      source: new XYZ({
+        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      }),
+    })
 
     // create feature layer and vector source
     var vectorLayer = new VectorLayer({
       source: new VectorSource({
-        features: []
-      })
-    });
+        features: [],
+      }),
+    })
 
     // create map object with feature layer
-     new Map({
+    new Map({
       target: 'map',
       layers: [raster, vectorLayer],
       view: new View({
         center: [0, 0],
-        zoom: 2
+        zoom: 2,
+      }),
+    })
+    this.setState({
+      map: map,
+      vectorLayer: vectorLayer,
+      colorMap: {},
+    })
+  }
+
+  addFeature(route) {
+    console.log('add', route)
+    var feature = new Feature({
+      geometry: new Point(fromLonLat([route['longitude'], route['latitude']])),
+    })
+    feature.setId(route['missionId'])
+
+    var style = this.state.colorMap[route['missionId']]
+    if (style == undefined) {
+      var num = Math.round(0xffffff * Math.random())
+      var r = num >> 16
+      var g = (num >> 8) & 255
+      var b = num & 255
+      var color = 'rgb(' + r + ', ' + g + ', ' + b + ')'
+      var style = new Style({
+        image: new Circle({
+          radius: 7,
+          fill: new Fill({ color: color }),
+          stroke: new Stroke({
+            color: 'black',
+            width: 2,
+          }),
+        }),
       })
-
-    });
-        this.setState({
-          map: map,
-          vectorLayer: vectorLayer,
-          colorMap: {}
-        });
-
+      this.state.colorMap[route['missionId']] = style
+    } else {
+      var style = this.state.colorMap[route['missionId']]
+      var color = style
+        .getImage()
+        .getFill()
+        .getColor()
     }
 
- addFeature(route){
-        console.log("add", route)
-         var feature = new Feature({
-               geometry: new Point(fromLonLat([route['longitude'], route['latitude']])),
-         });
-          feature.setId(route['missionId'])
+    feature.setStyle(style)
+    var oldPoint = this.state.vectorLayer
+      .getSource()
+      .getFeatureById(route['missionId'])
+    if (oldPoint != undefined) {
+      this.state.vectorLayer.getSource().removeFeature(oldPoint)
+    }
+    this.state.vectorLayer.getSource().addFeature(feature)
 
-var color= this.state.colorMap[route['missionId']]
-             if(color == undefined) {
-         var num = Math.round(0xffffff * Math.random());
-         var r = num >> 16;
-         var g = num >> 8 & 255;
-         var b = num & 255;
-         var color = 'rgb(' + r + ', ' + g + ', ' + b + ')'
-                   var style = new Style({
-                      image: new Circle({
-                        radius: 7,
-                        fill: new Fill({color: color}),
-                        stroke: new Stroke({
-                          color: 'black', width: 2
-                        })
-                      })
-                    })
-        this.state.colorMap[route['missionId']]=style
-}else{
-var style = this.state.colorMap[route['missionId']]
-var color=color.getImage().getFill().getColor()
-}
+    //add line
 
+    var line = this.state.vectorLayer
+      .getSource()
+      .getFeatureById(route['missionId'] + '-line')
+    if (line == undefined) {
+      var line = new Feature({
+        geometry: new LineString([
+          fromLonLat([route['longitude'], route['latitude']]),
+        ]),
+      })
+      line.setId(route['missionId'] + '-line')
 
-         feature.setStyle(style);
-         var v = this.state.vectorLayer.getSource().getFeatureById(route['missionId'])
-             if(v != undefined) {
-         this.state.vectorLayer.getSource().removeFeature(v)
-         }
-         this.state.vectorLayer.getSource().addFeature(feature)
+      var lineStyle = new Style({
+        stroke: new Stroke({
+          color: color,
+          width: 2,
+        }),
+      })
+      line.setStyle(lineStyle)
 
-
-
-         //add line
-
-var line =  this.state.vectorLayer.getSource().getFeatureById(route['missionId']+'-line')
-    if(line != undefined) {
-        var geo=line.getGeometry()
-        geo.appendCoordinate(fromLonLat([route['longitude'], route['latitude']]));
-        return;
-}
-          var line = new Feature({
-                        geometry: new LineString( [fromLonLat([route['longitude'], route['latitude']])]
-),
-                  });
-          line.setId(route['missionId']+'-line')
-
-          var lineStyle = new Style({
-            stroke: new Stroke({
-              color: color,
-              width: 2
-            })
-          });
-                             line.setStyle(lineStyle);
-
-         this.state.vectorLayer.getSource().addFeature(line)
-
-
-
- }
+      this.state.vectorLayer.getSource().addFeature(line)
+    } else {
+      var geo = line.getGeometry()
+      geo.appendCoordinate(fromLonLat([route['longitude'], route['latitude']]))
+    }
+  }
 
   // pass new features from props into the OpenLayers layer object
   componentDidUpdate(prevProps, prevState) {
     var routes = this.props.routes
-    if(routes != undefined) {
-        routes.map(this.addFeature, {state: this.state});
+    if (routes != undefined) {
+      routes.map(this.addFeature, { state: this.state })
     }
   }
 
-  render () {
-    return (
-      <div id="map"> </div>
-    );
+  render() {
+    return <div id="map"> </div>
   }
 }
 
 class App extends Component {
-    constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-        contacts: []
-    };
+      routes: [],
     }
-
-
+  }
 
   tick() {
-    fetch('/services/healthstatus/pulse/v1/status/sensor/geo',
-        {method:'GET', credentials: 'same-origin', cache: 'no-cache',
-         headers: {
-         'X-Requested-With': 'XMLHttpRequest',
-         'Authorization': 'Basic ' + "YWRtaW46YWRtaW4="
-},
-        })
-            .then(res => res.json())
-            .then((data) => {
-                console.log(data)
-                this.setState({ contacts: data })
-            })
-            .catch(console.log)
+    fetch('/services/healthstatus/pulse/v1/status/sensor/geo', {
+      method: 'GET',
+      credentials: 'same-origin',
+      cache: 'no-cache',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+        Authorization: 'Basic ' + 'YWRtaW46YWRtaW4=',
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ routes: data })
+      })
+      .catch(console.log)
+  }
 
-          }
+  componentDidMount() {
+    this.interval = setInterval(() => this.tick(), 500)
+  }
 
-          componentDidMount() {
-            this.interval = setInterval(() => this.tick(), 500);
-          }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
 
-          componentWillUnmount() {
-            clearInterval(this.interval);
-          }
+  render() {
+    return (
+      <div>
+            <div style={{height: "60%"}}>
 
+        <MapWrapper routes={this.state.routes} />
+              </div>
+            <div style={{height: "35%"}}>
 
-/*
-                <Contacts contacts={this.state.contacts} map={this.props.map} />
-*/
-        render() {
-            return (
-            <div>
-                <MapWrapper routes={this.state.contacts}  />
-            </div>
-            )
-        }
+        <RouteTable routes={this.state.routes}  />
+              </div>
+
+      </div>
+    )
+  }
 }
 
-export {
-  App,
-  MapWrapper,
-}
+export { App, MapWrapper }
--- src/main/webapp/components/missions.js
+++ src/main/webapp/components/missions.js
@@ -1,22 +1,150 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+
+interface Column {
+  id: 'name' | 'code' | 'population' | 'size' | 'density';
+  label: string;
+  minWidth?: number;
+  align?: 'right';
+  format?: (value: number) => string;
+}
+
+const columns: Column[] = [
+  { id: 'name', label: 'Name', minWidth: 170 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  {
+    id: 'population',
+    label: 'Population',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'size',
+    label: 'Size\u00a0(km\u00b2)',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toLocaleString(),
+  },
+  {
+    id: 'density',
+    label: 'Density',
+    minWidth: 170,
+    align: 'right',
+    format: (value: number) => value.toFixed(2),
+  },
+];
+
+interface Data {
+  name: string;
+  code: string;
+  population: number;
+  size: number;
+  density: number;
+}
+
+function createData(name: string, code: string, population: number, size: number): Data {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+/*const rows = [
+  createData('India', 'IN', 1324171354, 3287263),
+  createData('China', 'CN', 1403500365, 9596961),
+  createData('Italy', 'IT', 60483973, 301340),
+  createData('United States', 'US', 327167434, 9833520),
+];*/
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+  },
+  container: {
+    maxHeight: '100%',
+  },
+});
 
 
-const Contacts = ({contacts}) => {
-    return (
-        <div>
-            <center><h1>Contact List</h1></center>
-                        <div id="map"></div>
+export default function StickyHeadTable(props) {
 
-            {contacts.map((contact, key) => (
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{contact.streamId}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">{contact.email}</h6>
-                    </div>
-                </div>
-            ))}
-        </div>
-    )
-};
+  const addRow = (route: unknown) => {
+return                 createData(route['missionId'], route['streamId'], 1324171354, 3287263)
 
-export default Contacts
+  };
+
+    var rows =  [];
+    var routes = props.routes
+    if (routes != undefined) {
+     rows = routes.map(addRow)
+     console.log(rows)
+    }
+
+
+   const classes = useStyles();
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map(column => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(row => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                  {columns.map(column => {
+                    const value = row[column.id];
+                    return (
+                      <TableCell key={column.id} align={column.align}>
+                        {column.format && typeof value === 'number' ? column.format(value) : value}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
